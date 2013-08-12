9f7cbf61705047a43a269c43fc9354d633c57951
Zhenya Kostev <zh.kostev@gmail.com>
2013-08-13 00:48:00 +0300

    Fix 500 in admin (set en locale for admin area)

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,4 +2,9 @@ class Admin::BaseController < ActionController::Base
   layout 'admin'
 
   before_filter :authenticate_admin_user!
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = :en
+  end
 end
