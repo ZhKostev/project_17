663dae4201e6e33f20c8102fb23cfb77f634b16b
Zhenya Kostev <zh.kostev@gmail.com>
2013-09-29 01:48:04 +0300

    Fix assets for ckeditor 4

--- app/assets/javascripts/admin/ckeditor_fix.js
+++ app/assets/javascripts/admin/ckeditor_fix.js
@@ -1,11 +1,5 @@
 var CKEDITOR_BASEPATH = '/assets/ckeditor/';
 
-(function() {
-  if (typeof window['CKEDITOR_BASEPATH'] === "undefined" || window['CKEDITOR_BASEPATH'] === null) {
-    window['CKEDITOR_BASEPATH'] = "<%= config.relative_url_root %>/assets/ckeditor/";
-  }
-}).call(this);
-
 function ck_load() {
   $('.ckeditor').each(function(){
     CKEDITOR.replace( $(this).attr('name') );
