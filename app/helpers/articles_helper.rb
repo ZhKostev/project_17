a6a2f2dcfd48d666b2a30555d66bc559e758a729
Zhenya Kostev <zh.kostev@gmail.com>
2013-10-30 23:59:49 -0400

    Fix article show page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,12 @@
 require 'coderay'
 module ArticlesHelper
   def coderay_wrapper(text)
-    text.gsub(/\<div lang="(.+?)"\>.+?\<pre\>(.+?)?\<\/pre\>\W*div\>/m) do
+    text = text.gsub('&amp;lt;','<').gsub('&amp;gt;','>').gsub('&#39;',"'").gsub('&amp;','&')
+    result = text.gsub(/\<div lang="(.+?)"\>.+?\<pre\>(.+?)?\<\/pre\>\W*div\>/m) do
       content_tag("notextile", CodeRay.scan($2, $1).div(:css => :class).html_safe)
-    end.html_safe.gsub('&amp;lt;','<').gsub('&amp;gt;','>').html_safe
+    end
+
+    result.gsub('&amp;lt;','<').gsub('&amp;gt;','>').gsub('&amp;#39;',"'").gsub('&amp;quot;','"').html_safe
   end
+
 end
